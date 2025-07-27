b9acf1e8027bd66642e34df104a015d936542992
SUBHRAJYOTI MANDAL <subhrajyotimandal1216@gmail.com>
2025-07-27 14:46:46 +0000

    initial commit

--- state.py
+++ state.py
@@ -1,33 +1,42 @@
 import reflex as rx
-import asyncio
 import os
 from openai import AsyncOpenAI
-
-client = AsyncOpenAI(
-    api_key="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"
-)
 
 
 class State(rx.State):
     question: str
     chat_history: list[tuple[str, str]]
 
+    @rx.event
     async def answer(self):
-        # Our chatbot is not very smart right now...
-        answer = "A way to build web apps in pure Python! Anything from a simple website to a complex web app!"
-        self.chat_history.append((self.question, ""))
+        # Our chatbot has some brains now!
+        client = AsyncOpenAI(api_key=os.environ["OPENAI_API_KEY"])
+
+        session = await client.chat.completions.create(
+            model="o3-mini",
+            messages=[{"role": "user", "content": self.question}],
+            stop=None,
+            temperature=0.7,
+            stream=True,
+        )
+
+        # Add to the answer as the chatbot responds.
+        answer = ""
+        self.chat_history.append((self.question, answer))
 
         # Clear the question input.
         self.question = ""
         # Yield here to clear the frontend input before continuing.
         yield
 
-        for i in range(len(answer)):
-            # Pause to show the streaming effect.
-            await asyncio.sleep(0.02)
-            # Add one letter at a time to the output.
-            self.chat_history[-1] = (
-                self.chat_history[-1][0],
-                answer[: i + 1],
-            )
-            yield
+        async for item in session:
+            if hasattr(item.choices[0].delta, "content"):
+                if item.choices[0].delta.content is None:
+                    # presence of 'None' indicates the end of the response
+                    break
+                answer += item.choices[0].delta.content
+                self.chat_history[-1] = (
+                    self.chat_history[-1][0],
+                    answer,
+                )
+                yield
