c0f11a9ba75b8c49e3cb89e3e56f6652bddcccb1
SUBHRAJYOTI MANDAL <subhrajyotimandal1216@gmail.com>
2025-08-17 20:28:31 +0000

    app fixed using huggingface inference

--- state.py
+++ state.py
@@ -1,11 +1,12 @@
 #state.py
 import reflex as rx
+import requests
+from huggingface_hub import InferenceClient
 import os
 import asyncio
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_ollama.llms import OllamaLLM
-from langchain_ollama.chat_models import ChatOllama
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -13,17 +14,22 @@ load_dotenv()
 
 os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
 os.environ["LANGCHAIN_TRACING"] = "true"
+HUGGINGFACE_TOKEN = os.getenv("HUGGINGFACE_API_TOKEN")
+MODEL_ID = "meta-llama/Llama-3.2-3B-Instruct"
+
+client = InferenceClient(model=MODEL_ID, token=HUGGINGFACE_TOKEN)
 
 prompt = ChatPromptTemplate.from_messages(
     [
-        ("system", "You are a helpful assistant. Please provide useful response to the user's queries"),
+        ("system", "You are BuddyAI, a concise and helpful AI assistant. When answering questions, provide clear"
+        "and complete responses that are medium in length — not too short or too long."
+        "Avoid unnecessary explanations or extra details unless the user asks for more."),
         ("user", "Question: {question}")
     ]
 )
 
-llm = OllamaLLM(model="llama3.2", streaming=True)
 parser = StrOutputParser()
-chain = prompt | llm | parser
+chain = prompt | parser
 
 class State(rx.State):
     question: str
@@ -36,9 +42,19 @@ class State(rx.State):
         self.question = ""
         yield
 
-        async for chunk in chain.astream({"question": question_copy}):
-            self.chat_history[-1] = (
-                self.chat_history[-1][0],
-                self.chat_history[-1][1] + chunk
-            )
+        formatted = prompt.invoke({"question": question_copy}).to_string()
+
+        # Streaming response
+        response_text = ""
+        stream = await asyncio.to_thread(
+            client.chat_completion,
+            messages=[{"role": "user", "content": formatted}],
+            stream=True,
+            max_tokens=2400,
+            model=MODEL_ID
+        )
+        for token in stream:
+            fragment = token.choices[0].delta.content or ""
+            response_text += fragment
+            self.chat_history[-1] = (question_copy, response_text)
             yield
